6437d0ea068735d212e10ef4e212a7e285e1c493
Milan Savickij <milan.savitskiy@email.cz>
2024-11-01 23:54:35 +0100

    gugu gaga

--- parser/openai_parser.py
+++ parser/openai_parser.py
@@ -1,3 +1,33 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+from os import getenv
+from pydantic import BaseModel
+from const import TEST_INPUT
 
-client = OpenAI()
+load_dotenv()
+
+_OPENAI_API_KEY = getenv('OPENAI_API_KEY')
+
+client = OpenAI(api_key=_OPENAI_API_KEY)
+
+class KeyValue(BaseModel):
+    original_tag: str
+    key: str
+    value: str
+
+class Response(BaseModel):
+    values: list[KeyValue]
+
+completion = client.beta.chat.completions.parse(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are supposed to export key value pairs from the given text and store their original tag."},
+        {
+            "role": "user",
+            "content": TEST_INPUT
+        }
+    ],
+    response_format=Response
+)
+
+print(completion.choices[0].message)
