bf730dd13576d06b663235248ab5ed2286896be3
Milan Savickij <milan.savitskiy@email.cz>
2024-11-02 09:08:56 +0100

    feat: reports queueing

--- parser/openai_parser.py
+++ parser/openai_parser.py
@@ -3,33 +3,36 @@ import json
 from openai import OpenAI
 from dotenv import load_dotenv
 from os import getenv
-from const import TEST_INPUT
 from report_types import Response
 
 load_dotenv()
 
-_OPENAI_API_KEY = getenv('OPENAI_API_KEY')
+def initialize_client() -> OpenAI:
+    _OPENAI_API_KEY = getenv('OPENAI_API_KEY')
 
-client = OpenAI(api_key=_OPENAI_API_KEY)
+    return OpenAI(api_key=_OPENAI_API_KEY)
 
-completion = client.beta.chat.completions.parse(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are supposed to export key value pairs from the given text and store their original tag."},
-        {
-            "role": "user",
-            "content": TEST_INPUT
-        }
-    ],
-    response_format=Response
-)
+def process_report(client: OpenAI, content: str, report_index: str):
 
-response_dict = completion.choices[0].message.dict()
-response_json = json.dumps(response_dict, indent=4)
+    completion = client.beta.chat.completions.parse(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a medical nurse reading patient's reports in czech language."},
+            {"role": "system", "content": "You are supposed to export key-value pairs from the given text and store their original tag."},
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        response_format=Response
+    )
 
-with open('response.json', 'w') as json_file:
-    json_file.write(response_json)
+    # response_dict = completion.choices[0].message.content
+    # response_json = json.dumps(response_dict, indent=4)
 
-print("Response saved to response.json")
+    with open(f'response-{report_index}.json', 'w') as json_file:
+        json_file.write(completion.choices[0].message.content)
 
-print(completion.choices[0].message)
+    print("Response saved to response.json")
+
+    # print(completion.choices[0].message)
--- parser/reports_processor.py
+++ parser/reports_processor.py
@@ -1,18 +1,26 @@
 from clean import iterate_latest_column
 from dotenv import load_dotenv
 from os import getenv
+from openai_parser import initialize_client, process_report
 
 load_dotenv()
 
 FILE_PATH=getenv('FILE_PATH')
 
+
 print(FILE_PATH)
 
 def process_reports():
+    client = initialize_client()
     for row in iterate_latest_column(FILE_PATH):
         print(row)
+        response_index = f'{row[0]}-{row[1]}-{row[3]}-{row[4]}'
+        report_text = row[-1]
+
         if(input('Continue?') == 'n'):
             return
+        
+        process_report(client, report_text, response_index)
     pass    
 
 process_reports()
