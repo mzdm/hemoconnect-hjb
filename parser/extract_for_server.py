bfd31daa169c1ec0eccd1eba91e9c724e07166dd
Matej Zidek <zidek.matt@gmail.com>
2024-11-02 16:30:29 +0100

    patient meta info

---
+++ parser/extract_for_server.py
@@ -0,0 +1,44 @@
+import csv
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+LAB_FILE_PATH = getenv('AMB_CLEANED_FILE_PATH')
+
+OUT_FILE_PATH_ID_PACS = '../server/data/id_pacs.csv'
+OUT_FILE_PATH_PATIENT_AMB = '../server/data/patient_amb.csv'
+
+def extract_id_pacs_columns_values():
+    with open(LAB_FILE_PATH, mode='r', encoding='windows-1250') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        headers = reader.fieldnames
+        rows = list(reader)
+
+        set_of_ic_pacs = set()
+        for row in rows:
+            set_of_ic_pacs.add(row['ic_pac'])
+
+        # print(set_of_ic_pacs)
+
+    # save to csv file named id_pacs.csv
+    with open(OUT_FILE_PATH_ID_PACS, mode='w', encoding='windows-1250', newline='') as outfile:
+        writer = csv.writer(outfile, delimiter=';')
+        writer.writerow(['ic_pac'])
+        for ic_pac in set_of_ic_pacs:
+            writer.writerow([ic_pac])
+
+def extract_patient_amb():
+    with open(LAB_FILE_PATH, mode='r', encoding='windows-1250') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        headers = reader.fieldnames
+        rows = list(reader)
+
+    with open(OUT_FILE_PATH_PATIENT_AMB, mode='w', encoding='windows-1250', newline='') as outfile:
+        writer = csv.writer(outfile, delimiter=';')
+        writer.writerow(['ic_pac', 'ic_amb_zad', 'i_dg_kod', 'i_text_dg'])
+        for row in rows:
+            writer.writerow([row['ic_pac'], row['ic_amb_zad'], row['i_dg_kod'] , row['i_text_dg']])
+
+extract_id_pacs_columns_values()
+extract_patient_amb()
--- server/http_server.py
+++ server/http_server.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 
 PORT = 8000
 ID_PACS_CSV_PATH = 'data/id_pacs.csv'
+PATIENT_AMB_CSV_PATH = 'data/patient_amb.csv'
 id_pacs_data = []
+patient_amb_data = []
 
-def load_id_pacs_data():
+def preload_data():
     global id_pacs_data
+    global patient_amb_data
     with open(ID_PACS_CSV_PATH, mode='r', encoding='windows-1250') as file:
         reader = csv.DictReader(file, delimiter=';')
         id_pacs_data = [row for row in reader]
+
+    with open(PATIENT_AMB_CSV_PATH, mode='r', encoding='windows-1250') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        patient_amb_data = [row for row in reader]
 
 @app.route('/api/query', methods=['POST'])
 def query_patient_ids():
@@ -20,6 +27,21 @@ def query_patient_ids():
     query_string = data.get('patientId', '')
     patient_ids = [row['ic_pac'] for row in id_pacs_data if row['ic_pac'].startswith(query_string)]
     return jsonify(patient_ids)
+
+@app.route('/api/query/<patientId>', methods=['POST'])
+def query_patient_details(patientId):
+    patient_details = [
+        {
+            'ic_pac': row['ic_pac'],
+            'ic_amb_zad': row['ic_amb_zad'],
+            'i_dg_kod': row['i_dg_kod'],
+            'i_text_dg': row['i_text_dg']
+        }
+        for row in patient_amb_data if row['ic_pac'] == patientId
+    ]
+    if not patient_details:
+        return jsonify({'error': 'Patient not found'}), 404
+    return jsonify(patient_details)
 
 @app.route('/api/hello', methods=['GET'])
 def say_hello():
@@ -34,5 +56,5 @@ def add_headers(response):
 
 if __name__ == '__main__':
     os.chdir(os.curdir)  # or set to your specific path
-    load_id_pacs_data()
+    preload_data()
     app.run(port=PORT, debug=True)
