25947b93b942b1b488cb216e56cdc069b4ea4f44
Milan Savickij <milan.savitskiy@email.cz>
2024-11-02 11:15:46 +0100

    small changes

---
+++ frontend/src/Schemes.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import FormSelect from "./components/form-select";
+import EnhancedSearchForm from "./components/enhanced-search-form";
+import { Card, CardHeader, CardTitle, CardDescription } from "./components/ui/card";
+import { FormSchema } from "./hooks/form-schema";
+import { PageLayout } from "./components/page-layout";
+
+const isValidIndex = (index: number | undefined | null): index is number =>
+    Number.isInteger(index) && typeof index === "number" && index >= 0;
+
+export function Schemes() {
+    const [selectedForm, setSelectForm] = useState<FormSchema>();
+    const [selectedIndex, setSelectIndex] = useState<number>();
+
+    return (
+        <PageLayout>
+        {!selectedForm && (
+        <>
+          <h2>Formuláře</h2>
+          <FormSelect
+            onFormSelect={(schema, index) => {
+              setSelectForm(schema);
+              setSelectIndex(index);
+            } } 
+          />
+        </>
+      )}
+      {selectedForm && isValidIndex(selectedIndex) && (
+        <Card className="w-full max-w-3xl mx-auto p-4 ">
+        {selectedForm && (
+          <button
+            onClick={() => setSelectForm(undefined)}
+            className="p-2 mt-4 ml-4 text-gray-600 hover:text-gray-900"
+          >
+            ← Zpátky
+          </button>
+        )}
+        <CardHeader>
+          <CardTitle>EMIS</CardTitle>
+          <CardDescription>Enhanced Medical Information Search</CardDescription>
+        </CardHeader>
+        <EnhancedSearchForm
+          initialFormState={selectedForm}
+          index={selectedIndex}
+        />
+        </Card>
+
+      )}
+      {/* {!selectedForm && (
+        <>
+          <div className="h-4" />
+          <h2>Políčka</h2>
+          <FormSelect
+            onFormSelect={(schema, index) => {
+              setSelectForm(schema);
+              setSelectIndex(index);
+            } } 
+          />
+        </>
+      )} */}
+      </PageLayout>
+    )
+}
---
+++ frontend/src/components/page-layout.tsx
@@ -0,0 +1,31 @@
+import { NavigationMenu, NavigationMenuLink } from "@radix-ui/react-navigation-menu";
+import { PropsWithChildren } from "react";
+import { useMatches } from "react-router-dom";
+
+export const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
+
+    const matches = useMatches()
+
+    console.log(matches)
+
+    return (
+      <div className="flex flex-col min-h-screen p-8">
+        <div className="flex items-end flex-row gap-16">
+          <div className="flex flex-col">
+            <h1 className="text-xl">EMA</h1>
+            <p className="text-sm text-gray-400">Enhanced Medical Aggregator</p>
+          </div>
+
+            <NavigationMenu className="flex gap-8">
+              <NavigationMenuLink className={`text-lg ${matches[0]?.pathname === '/' ? 'text-cyan-600' : undefined}`} href="/">Přehled</NavigationMenuLink>
+              <NavigationMenuLink className={`text-lg ${matches[0]?.pathname === '/schemes' ? 'text-cyan-600' : undefined}`} href="/schemes">Schémata</NavigationMenuLink>
+            </NavigationMenu>
+        </div>
+  
+        
+        <div className="h-8" />
+
+        {children}
+      </div>
+    )
+}
