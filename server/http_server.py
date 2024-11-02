a3af944df20fb4a39395c69dbfda0378a6f81c97
Matej Zidek <zidek.matt@gmail.com>
2024-11-02 15:17:52 +0100

    i

--- server/http_server.py
+++ server/http_server.py
@@ -21,7 +21,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Methods', '*')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         super().end_headers()
 
