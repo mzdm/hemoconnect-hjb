5589a607a1678cf220a67b66ac639752ccf92265
Milan Savickij <milan.savitskiy@email.cz>
2024-11-02 09:15:18 +0100

    feat: add types

--- parser/report_types.py
+++ parser/report_types.py
@@ -5,5 +5,21 @@ class KeyValue(BaseModel):
     key: str
     value: str
 
+class KeyValueWithMeta():
+    original_tag: str
+    key: str
+    value: str
+    type: str
+
+    has_unit: bool
+    unit: str
+
+class ReportWithMeta():
+    values: list[KeyValueWithMeta]
+
+    patient_id: str
+    doctor_id: str
+    report_date: str
+
 class Response(BaseModel):
     values: list[KeyValue]
