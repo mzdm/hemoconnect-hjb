9fc2f58189180d5fd2c10ca3d8f40c73d779b592
Tomáš Kebrle <dev@tomaskebrle.cz>
2024-11-03 00:29:07 +0100

    if it does not work hardcode it 3: ecstatic trigaloo goes brrrr

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,96 +1,131 @@
-import { Search, SlidersHorizontal, SquareArrowOutUpRight, User } from "lucide-react";
+import {
+  Search,
+  SlidersHorizontal,
+  SquareArrowOutUpRight,
+  User,
+} from "lucide-react";
 import React, { KeyboardEvent } from "react";
 import { PageLayout } from "./components/page-layout";
 import { Button } from "./components/ui/button";
 import { Card, CardHeader } from "./components/ui/card";
 import { Input } from "./components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table";
-import { useQuery } from '@tanstack/react-query'
-import { useDebouncedCallback } from 'use-debounce'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+import { useDebouncedCallback } from "use-debounce";
 import { useNavigate } from "react-router-dom";
 
 function App() {
-  const searchRef = React.useRef('')
-
+  const searchRef = React.useRef("");
 
   const { data, refetch: refetchAutocomplete } = useQuery({
-    queryKey: ['autocomplete'],
+    queryKey: ["autocomplete"],
     initialData: [],
     queryFn: async (): Promise<string[]> => {
-      const data = await fetch(`${import.meta.env.VITE_API_URL}/query`, {
+      const data = await fetch(`https://api.hjb.mesh.sk/api/query/query`, {
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         method: "POST",
         body: JSON.stringify({ patientId: searchRef.current }),
-      })
+      });
 
-      const _data = await data.json()
+      const _data = await data.json();
 
-      return typeof data !== "undefined"  ? _data : []
-    }
-  })
+      return typeof data !== "undefined" ? _data : [];
+    },
+  });
 
-  const debounced = useDebouncedCallback(async () => {
-    refetchAutocomplete()
-  }, 250, {})
+  const debounced = useDebouncedCallback(
+    async () => {
+      refetchAutocomplete();
+    },
+    250,
+    {}
+  );
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <PageLayout>
       <h3 className="text-lg">Pacienti</h3>
       <div className="flex gap-2">
-        <Input placeholder="Začněte psát IC pacienta" onKeyDown={(e: KeyboardEvent<HTMLInputElement> & { target: { value: string }}) => {
-          searchRef.current = e.target.value
+        <Input
+          placeholder="Začněte psát IC pacienta"
+          onKeyDown={(
+            e: KeyboardEvent<HTMLInputElement> & { target: { value: string } }
+          ) => {
+            searchRef.current = e.target.value;
 
-          if (e.key === 'Enter') {
-            refetchAutocomplete()
-            return
-          }
+            if (e.key === "Enter") {
+              refetchAutocomplete();
+              return;
+            }
 
-          debounced()
-        }} />
-        <Button variant="outline" onClick={() => refetchAutocomplete()}><Search /></Button>
+            debounced();
+          }}
+        />
+        <Button variant="outline" onClick={() => refetchAutocomplete()}>
+          <Search />
+        </Button>
       </div>
       <div className="h-4" />
       <div className="flex items-start justify-start w-full gap-4">
         <Card className="w-3/6">
           <CardHeader className="flex justify-between items-center w-full flex-row">
             <span>Vyhledávání pacientů</span>
-            <Button className="flex items-center justify-center" variant="outline">
+            <Button
+              className="flex items-center justify-center"
+              variant="outline"
+            >
               <SlidersHorizontal />
             </Button>
           </CardHeader>
-          {data?.length > 0 && 
-          <Table>
-            <TableHeader>
-              <TableRow>
-              <TableHead className="w-[40px]"></TableHead>
-                <TableHead className="w-[100px]">ic pacienta</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data?.slice(0, 10).map((patientId) => (
-                <TableRow key={patientId}>
-                  <TableCell><User size={16} /></TableCell>
-                  <TableCell className="font-medium">{patientId}</TableCell>
-                  <TableCell>
-                    <Button variant="outline" onClick={() => navigate(`/patient/${patientId}`)}>Otevřít <SquareArrowOutUpRight /></Button>
-                  </TableCell>
+          {data?.length > 0 && (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[40px]"></TableHead>
+                  <TableHead className="w-[100px]">ic pacienta</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        }
+              </TableHeader>
+              <TableBody>
+                {data?.slice(0, 10).map(patientId => (
+                  <TableRow key={patientId}>
+                    <TableCell>
+                      <User size={16} />
+                    </TableCell>
+                    <TableCell className="font-medium">{patientId}</TableCell>
+                    <TableCell>
+                      <Button
+                        variant="outline"
+                        onClick={() => navigate(`/patient/${patientId}`)}
+                      >
+                        Otevřít <SquareArrowOutUpRight />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
         </Card>
         <Card className="w-3/6">
           <CardHeader className="flex justify-between items-center w-full flex-row">
-              <span>Statistiky</span>
-              <Button className="flex items-center justify-center" variant="outline">
-                <SlidersHorizontal />
-              </Button>
-            </CardHeader>
+            <span>Statistiky</span>
+            <Button
+              className="flex items-center justify-center"
+              variant="outline"
+            >
+              <SlidersHorizontal />
+            </Button>
+          </CardHeader>
         </Card>
       </div>
     </PageLayout>
